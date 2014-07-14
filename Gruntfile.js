a63d282108b713c0e245923cf753f37eeddf5ac1
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-07-14 11:45:26 +0300

    поменял пути

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function(grunt) {
         sass: {
             dist: {
                 options: {
-                    style: 'compressed'
+                    style: 'expanded'
                 },
                 files: {
                     'src/css/build/style.css': 'src/css/style.scss'
